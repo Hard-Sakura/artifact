718734cb59cd5b8aa0ffbfe461ca6f397a99377c
sunql <1450155238@qq.com>
2021-11-03 17:01:30 +0800

    third commit

--- src/main/java/com/sakura/artifact/init/ModCreativeTab.java
+++ src/main/java/com/sakura/artifact/init/ModCreativeTab.java
@@ -1,18 +1,24 @@
 package com.sakura.artifact.init;
 
+import com.sakura.artifact.blocks.ModBlocks;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/**
+ * @desc: artifactMiscTab
+ * @auther: SUNql
+ * @date: 2021/11/3 16:54
+ */
 public class ModCreativeTab {
-	public static final CreativeTabs IDL_MISC = new CreativeTabs(CreativeTabs.getNextID(), "ideallandMiscTab")
+	public static final CreativeTabs IDL_MISC = new CreativeTabs(CreativeTabs.getNextID(), "artifactMiscTab")
 	{
         @SideOnly(Side.CLIENT)
         public ItemStack getTabIconItem()
         {
-            return new ItemStack(Items.QUARTZ);
+            return new ItemStack(ModBlocks.ARTIFACT_K1);
         }
     };
 }
